13a14a612cfa7aebabb0e4445fd03d63cee1d007
Julian Hartmann <julian.hartmann@students.ksba.ch>
2022-08-16 23:16:45 +0200

    functionality of userinput

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,6 +1,28 @@
 $(document).ready(function () {
+  var count = 0;
+
   $("td").click(function () {
-    $(".field").prepend('<img src="../Images/circle.png" />');
+    //number of clicks
+    count++;
+    $("#num").text(count);
+
+    //changing between
+    if ($(this).is(':empty')) {
+      if (count % 2 == 0) {
+        $(this).prepend('<img src="../Images/x.png" />');
+      } else if (count % 2 != 0) {
+        $(this).prepend('<img src="../Images/circle.png" />');
+      }
+    }
+
+    /* if ($("img").width) {
+    } else if ($("img").width == 0) {
+      if (count % 2 == 0) {
+        $(this).prepend('<img src="../Images/x.png" />');
+      } else if (count % 2 != 0) {
+        $(this).prepend('<img src="../Images/circle.png" />');
+      }
+    } */
   });
 
   //if schleife
