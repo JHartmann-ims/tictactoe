85d08eb022c21c5f1ac0a1e9247f933be0f86d3a
Julian Hartmann <julian.hartmann@students.ksba.ch>
2022-09-03 23:45:23 +0200

    restart function kinda made

--- Scripts/index.js
+++ Scripts/index.js
@@ -15,18 +15,14 @@ $(document).ready(function () {
           $(this).prepend('<img class="circle" src="Images/circle.png" />');
         }
       } else if (won == true) {
-
       }
     }
 
-    //http://sstut.com/jquery/check-if-element-contains-another.php
     if (
-      ($("#field1").has(".circle").length &&
-        $("#field2").has(".circle").length &&
-        $("#field3").has(".circle").length) ||
-      ($("#field1").has(".x").length &&
-        $("#field2").has(".x").length &&
-        $("#field3").has(".x").length)
+      //fields with circle
+      $("#field1").has(".circle").length &&
+      $("#field2").has(".circle").length &&
+      $("#field3").has(".circle").length
     ) {
       $("#field1").css("background-color", "green");
       $("#field2").css("background-color", "green");
@@ -36,95 +32,152 @@ $(document).ready(function () {
 
       $(".inner-container")
         .children()
-        .css({ opacity: 0.60 })
+        .css({ opacity: 0.6 })
         .end()
-        .css({ opacity: 0.60 });
+        .css({ opacity: 0.6 });
 
-        $("<div id='wonDisplay'>YOU WON</div>").css({
+      $("<div id='wonDisplay'><p>You Won<p></div>")
+        .css({
           position: "absolute",
-          width: "100%",
-          height: "100%",
-          top: 0,
-          left: 0,
+          width: "60%",
+          height: "25%",
+          background: "grey",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          border: "3px solid black",
+        })
+        .appendTo($(".game-container").css("position", "relative"));
 
-        }).appendTo($(".game-container").css("position", "relative"));
+      $("<button type='button' class='btn btn-dark restartBtn'>Restart</button>").appendTo(
+        $("#wonDisplay")
+      );
+
+        $(".restartBtn").click(function () {
+          location.reload(true);
+        });
+
     } else if (
-      ($("#field4").has(".circle").length &&
-        $("#field5").has(".circle").length &&
-        $("#field6").has(".circle").length) ||
-      ($("#field4").has(".x").length &&
-        $("#field5").has(".x").length &&
-        $("#field6").has(".x").length)
+      $("#field4").has(".circle").length &&
+      $("#field5").has(".circle").length &&
+      $("#field6").has(".circle").length
     ) {
       $("#field4").css("background-color", "green");
       $("#field5").css("background-color", "green");
       $("#field6").css("background-color", "green");
     } else if (
-      ($("#field7").has(".circle").length &&
-        $("#field8").has(".circle").length &&
-        $("#field9").has(".circle").length) ||
-      ($("#field7").has(".x").length &&
-        $("#field8").has(".x").length &&
-        $("#field9").has(".x").length)
+      $("#field7").has(".circle").length &&
+      $("#field8").has(".circle").length &&
+      $("#field9").has(".circle").length
     ) {
       $("#field7").css("background-color", "green");
       $("#field8").css("background-color", "green");
       $("#field9").css("background-color", "green");
     } else if (
-      ($("#field1").has(".circle").length &&
-        $("#field4").has(".circle").length &&
-        $("#field7").has(".circle").length) ||
-      ($("#field1").has(".x").length &&
-        $("#field4").has(".x").length &&
-        $("#field7").has(".x").length)
+      $("#field1").has(".circle").length &&
+      $("#field4").has(".circle").length &&
+      $("#field7").has(".circle").length
     ) {
       $("#field1").css("background-color", "green");
       $("#field4").css("background-color", "green");
       $("#field7").css("background-color", "green");
     } else if (
-      ($("#field2").has(".circle").length &&
-        $("#field5").has(".circle").length &&
-        $("#field8").has(".circle").length) ||
-      ($("#field2").has(".x").length &&
-        $("#field5").has(".x").length &&
-        $("#field8").has(".x").length)
+      $("#field2").has(".circle").length &&
+      $("#field5").has(".circle").length &&
+      $("#field8").has(".circle").length
     ) {
       $("#field2").css("background-color", "green");
       $("#field5").css("background-color", "green");
       $("#field8").css("background-color", "green");
     } else if (
-      ($("#field3").has(".circle").length &&
-        $("#field6").has(".circle").length &&
-        $("#field9").has(".circle").length) ||
-      ($("#field3").has(".x").length &&
-        $("#field6").has(".x").length &&
-        $("#field9").has(".x").length)
+      $("#field3").has(".circle").length &&
+      $("#field6").has(".circle").length &&
+      $("#field9").has(".circle").length
     ) {
       $("#field3").css("background-color", "green");
       $("#field6").css("background-color", "green");
       $("#field9").css("background-color", "green");
     } else if (
-      ($("#field1").has(".circle").length &&
-        $("#field5").has(".circle").length &&
-        $("#field9").has(".circle").length) ||
-      ($("#field1").has(".x").length &&
-        $("#field5").has(".x").length &&
-        $("#field9").has(".x").length)
+      $("#field1").has(".circle").length &&
+      $("#field5").has(".circle").length &&
+      $("#field9").has(".circle").length
     ) {
       $("#field1").css("background-color", "green");
       $("#field5").css("background-color", "green");
       $("#field9").css("background-color", "green");
     } else if (
-      ($("#field7").has(".circle").length &&
-        $("#field5").has(".circle").length &&
-        $("#field3").has(".circle").length) ||
-      ($("#field7").has(".x").length &&
-        $("#field5").has(".x").length &&
-        $("#field3").has(".x").length)
+      $("#field7").has(".circle").length &&
+      $("#field5").has(".circle").length &&
+      $("#field3").has(".circle").length
     ) {
       $("#field7").css("background-color", "green");
       $("#field5").css("background-color", "green");
       $("#field3").css("background-color", "green");
+    } else if (
+      //fields with x
+      $("#field1").has(".x").length &&
+      $("#field2").has(".x").length &&
+      $("#field3").has(".x").length
+    ) {
+      $("#field1").css("background-color", "green");
+      $("#field2").css("background-color", "green");
+      $("#field3").css("background-color", "green");
+    } else if (
+      $("#field4").has(".x").length &&
+      $("#field5").has(".x").length &&
+      $("#field6").has(".x").length
+    ) {
+      $("#field4").css("background-color", "green");
+      $("#field5").css("background-color", "green");
+      $("#field6").css("background-color", "green");
+    } else if (
+      $("#field7").has(".x").length &&
+      $("#field8").has(".x").length &&
+      $("#field9").has(".x").length
+    ) {
+      $("#field7").css("background-color", "green");
+      $("#field8").css("background-color", "green");
+      $("#field9").css("background-color", "green");
+    } else if (
+      $("#field1").has(".x").length &&
+      $("#field5").has(".x").length &&
+      $("#field9").has(".x").length
+    ) {
+      $("#field1").css("background-color", "green");
+      $("#field5").css("background-color", "green");
+      $("#field9").css("background-color", "green");
+    } else if (
+      $("#field3").has(".x").length &&
+      $("#field5").has(".x").length &&
+      $("#field7").has(".x").length
+    ) {
+      $("#field3").css("background-color", "green");
+      $("#field5").css("background-color", "green");
+      $("#field7").css("background-color", "green");
+    } else if (
+      $("#field1").has(".x").length &&
+      $("#field4").has(".x").length &&
+      $("#field7").has(".x").length
+    ) {
+      $("#field1").css("background-color", "green");
+      $("#field4").css("background-color", "green");
+      $("#field7").css("background-color", "green");
+    } else if (
+      $("#field2").has(".x").length &&
+      $("#field5").has(".x").length &&
+      $("#field8").has(".x").length
+    ) {
+      $("#field2").css("background-color", "green");
+      $("#field5").css("background-color", "green");
+      $("#field8").css("background-color", "green");
+    } else if (
+      $("#field3").has(".x").length &&
+      $("#field6").has(".x").length &&
+      $("#field9").has(".x").length
+    ) {
+      $("#field3").css("background-color", "green");
+      $("#field6").css("background-color", "green");
+      $("#field9").css("background-color", "green");
     } else if (
       $("#field1").has("img").length &&
       $("#field2").has("img").length &&
@@ -139,9 +192,6 @@ $(document).ready(function () {
       $(
         "#field1, #field2, #field3, #field4, #field5, #field6, #field7, #field8, #field9"
       ).css("background-color", "pink");
-      //td
     }
   });
-
-  //if schleife
 });
